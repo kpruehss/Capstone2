6175fa2571ef7e4144679e54e68e16a4ac37a887
Karsten Pruehss <Karsten.Pruehss@xakiatech.com>
2021-01-01 12:01:15 +1100

    First commit

---
+++ Capstone2/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+
+[<EntryPoint>]
+let main argv =
+    0 // return an integer exit code
