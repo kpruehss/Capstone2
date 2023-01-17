c3fea03860f64a324146bbb5202adcad9a570851
Karsten Pruehss <Karsten.Pruehss@xakiatech.com>
2023-01-17 11:04:43 +1100

    add capstone 3

--- Capstone2/Capstone3Scratch.fsx
+++ Capstone2/Capstone3Scratch.fsx
@@ -1,4 +1,25 @@
 #load "Domain.fs"
+#load "Operations.fs"
+
+open Capstone2.Domain
+open Capstone2.Operations
+open System
+
+let isValidCommand = (Set [ 'w'; 'd'; 'x' ]).Contains
+
+let isStopCommand = (=) 'x'
+
+let getAmount (command: char) =
+    match command with
+    | 'd' -> 'd', 50M
+    | 'w' -> 'w', 25M
+    | _ -> command, 0M
+
+let processCommand account (command, amount) =
+    if command = 'd' then
+        account |> deposit amount
+    else
+        account |> withdraw amount
 
 let openingAccount =
     { Owner = { Name = "Karsten" }
--- Capstone2/Program.fs
+++ Capstone2/Program.fs
@@ -3,10 +3,25 @@
 open System
 open Capstone2.Domain
 open Capstone2.Operations
-// Define a function to construct a message to print
+
+let isValidCommand = (Set [ 'w'; 'd'; 'x' ]).Contains
+
+let isStopCommand = (=) 'x'
+
+let getAmount (command: char) =
+    match command with
+    | 'd' -> 'd', 50M
+    | 'w' -> 'w', 25M
+    | _ -> command, 0M
+
+let processCommand account (command, amount) =
+    if command = 'd' then
+        account |> deposit amount
+    else
+        account |> withdraw amount
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     let name =
         Console.Write "Please enter your name: "
         Console.ReadLine()
@@ -24,10 +39,34 @@ let main argv =
 
     let closingAccount =
         // Fill in the main loop here...
+        let openingAccount =
+            { Owner = { Name = "Karsten" }
+              Balance = 0M
+              AccountId = Guid.Empty }
+
+        let commands =
+            seq {
+                while true do
+                    Console.Write "(d)eposit, (w)ithdraw or e(x)it: "
+                    yield Console.ReadKey().KeyChar
+                    Console.WriteLine()
+            }
+
+        let getAmount command =
+            Console.WriteLine()
+            Console.Write "Enter Amount: "
+            command, Console.ReadLine() |> Decimal.Parse
+
+        // commands
+        // |> Seq.filter isValidCommand
+        // |> Seq.takeWhile (not << isStopCommand)
+        // |> Seq.map getAmount
+        // |> Seq.fold processCommand openingAccount
+
         openingAccount
 
     Console.Clear()
-    printfn $"Closing Balance:\r\n {closingAccount}"
+    printfn "Closing Balance:\r\n %A" closingAccount
     Console.ReadKey() |> ignore
 
-    0 // return an integer exit code
+    0
